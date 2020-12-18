ce0742bc39e2e966c6a5976098c3299983a53f0a
Rachel Skwerer <rachelskwerer@gmail.com>
2020-12-17 21:46:28 -0500

    secondary commit

--- script.js
+++ script.js
@@ -110,29 +110,3 @@ window.addEventListener("load",function() {
 
 });
 
-// 
-/*
-fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
-   response.json().then(function(json){
-       const astronauts = document.getElementById('astronauts');
-       const count = document.getElementById('count');
-       
-       const div = document.getElementById("atronauts");
-       div.innerHTML = `
-       <div class = "bio">
-       <h3>${astronaut.firstName} - ${astronaut.lastName}</h3>
-           <ul> 
-               <li>Hours in space: ${astronaut.hoursInSpace}</li>
-               <li>Active: ${astronaut.active}</li>
-               <li>Skills:${astronaut.skills.join(", ")}</li>
-           </ul>
-       </div>
-       `
-  
-   });
-});
-});*/
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-
-*/
